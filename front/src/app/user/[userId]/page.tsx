319face3debebcd5d1d1bee57e81dd25a1da180c
sub9707 <110171787+sub9707@users.noreply.github.com>
2023-04-18 10:59:29 +0900

    fix user Routes

--- front/src/app/user/[userId]/page.tsx
+++ front/src/app/user/[userId]/page.tsx
@@ -1,6 +1,6 @@
 import styles from "./page.module.scss";
 
-export default function Home() {
+export default function Page({ params }: { params: { slug: string } }) {
   return (
     <>
       <div className={styles.pageContainer}>
