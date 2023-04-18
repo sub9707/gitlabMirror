50a1ea10b776ca44a3064df98dc12e7f701bdd06
sub9707 <110171787+sub9707@users.noreply.github.com>
2023-04-18 09:47:40 +0900

    feat header

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -5,23 +5,31 @@ import Image from "next/image";
 
 export default function Header() {
   return (
-    <header className={styles.headerWrap}>
-      <nav className={styles.navMenu}>
-        <Link href="/" className={styles.linkTag}>
-          home
-        </Link>
-        <Link href="/" className={styles.linkTag}>
-          about
-        </Link>
-        <Link href="/" className={styles.linkTag}>
-          posts
-        </Link>
-      </nav>
-      <Image src={Logo} alt="logo" placeholder="blur" />
-      <nav className={styles.navIcons}>
-        <Link href="/">home</Link>
-        <Link href="/">about</Link>
-      </nav>
+    <header className={styles.navContainer}>
+      <div className={styles.headerWrap}>
+        <nav className={styles.navMenu}>
+          <Link href="/" className={styles.linkTag}>
+            home
+          </Link>
+          <Link href="/" className={styles.linkTag}>
+            about
+          </Link>
+          <Link href="/" className={styles.linkTag}>
+            posts
+          </Link>
+        </nav>
+        <Image
+          src={Logo}
+          alt="logo"
+          placeholder="blur"
+          className={styles.logoImg}
+        />
+        <nav className={styles.navIcons}>
+          <Link href="/">아이콘1</Link>
+          <Link href="/">아이콘2</Link>
+        </nav>
+      </div>
+      <div className={styles.divider} />
     </header>
   );
 }
