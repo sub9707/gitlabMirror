8e882562f6b24b2e464983363f5825b4806da1df
sub9707 <110171787+sub9707@users.noreply.github.com>
2023-04-18 10:51:26 +0900

    feat page add

---
+++ front/src/app/userPage/page.tsx
@@ -0,0 +1,11 @@
+import styles from "./page.module.scss";
+
+export default function Home() {
+  return (
+    <>
+      <div className={styles.pageContainer}>
+        <div className={styles.profileWrapper}></div>
+      </div>
+    </>
+  );
+}
