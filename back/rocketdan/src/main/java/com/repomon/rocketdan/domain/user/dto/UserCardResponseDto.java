ea5f088659c197c5e6ea5caaa48c2788181682f4
Kminis <hello11robot@gmail.com>
2023-04-19 15:20:46 +0900

    [BE] feat : Dto Method 목업 작성
    - fromEntity, fromEntityList 작성

--- back/rocketdan/src/main/java/com/repomon/rocketdan/domain/user/dto/UserCardResponseDto.java
+++ back/rocketdan/src/main/java/com/repomon/rocketdan/domain/user/dto/UserCardResponseDto.java
@@ -1,10 +1,14 @@
 package com.repomon.rocketdan.domain.user.dto;
 
 
+import com.repomon.rocketdan.domain.user.entity.UserCardEntity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Data
@@ -12,4 +16,18 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class UserCardResponseDto {
+
+	public static UserCardResponseDto fromEntity(UserCardEntity userCard) {
+		return new UserCardResponseDto();
+	}
+
+
+	public static List<UserCardResponseDto> fromEntityList(List<UserCardEntity> userCardList) {
+		List<UserCardResponseDto> result = new ArrayList<>();
+		for (UserCardEntity userCard : userCardList) {
+			UserCardResponseDto userCardResponseDto = UserCardResponseDto.fromEntity(userCard);
+			result.add(userCardResponseDto);
+		}
+		return result;
+	}
 }
--- back/rocketdan/src/main/java/com/repomon/rocketdan/domain/user/dto/UserLanguageResponseDto.java
+++ back/rocketdan/src/main/java/com/repomon/rocketdan/domain/user/dto/UserLanguageResponseDto.java
@@ -1,10 +1,14 @@
 package com.repomon.rocketdan.domain.user.dto;
 
 
+import com.repomon.rocketdan.domain.user.entity.UserLanguageEntity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Data
@@ -12,4 +16,18 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class UserLanguageResponseDto {
+
+	public static UserLanguageResponseDto fromEntity(UserLanguageEntity userLanguage) {
+		return new UserLanguageResponseDto();
+	}
+
+
+	public static List<UserLanguageResponseDto> fromEntityList(List<UserLanguageEntity> userLanguageList) {
+		List<UserLanguageResponseDto> result = new ArrayList<>();
+		for (UserLanguageEntity userLanguage : userLanguageList) {
+			UserLanguageResponseDto userLanguageResponseDto = UserLanguageResponseDto.fromEntity(userLanguage);
+			result.add(userLanguageResponseDto);
+		}
+		return result;
+	}
 }
