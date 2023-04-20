9dc4b38c6a7831efe39a03ab2dec9252c1229737
백준봉 <bjb2992@naver.com>
2023-04-20 16:57:00 +0900

    Feat : 레포 기본 정보 추가

--- front/src/app/user/[userId]/[repoId]/page.tsx
+++ front/src/app/user/[userId]/[repoId]/page.tsx
@@ -4,7 +4,9 @@ import React, { useEffect, useRef } from "react";
 import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import ProgressBar from "components/ProgressBar";
 import styles from "./page.module.scss";
+import { StarIcon, ShareIcon } from "@heroicons/react/24/outline";
 
 function Page({ params }: { params: { repoId: string } }) {
   const canvas = useRef<HTMLDivElement>(null);
@@ -12,11 +14,12 @@ function Page({ params }: { params: { repoId: string } }) {
   useEffect(() => {
     const scene = new THREE.Scene();
 
-    const loader = new GLTFLoader();
+    const loader1 = new GLTFLoader();
 
-    loader.load(
+    loader1.load(
       "/cute_fox.glb",
       function (gltf) {
+        gltf.scene.position.set(0, 0, 0);
         scene.add(gltf.scene);
       },
       undefined,
@@ -25,9 +28,27 @@ function Page({ params }: { params: { repoId: string } }) {
       }
     );
 
-    const light = new THREE.PointLight(0xffffff, 4.5, 100);
-    light.position.set(0, 0, 50);
+    const loader2 = new GLTFLoader();
+
+    loader2.load(
+      "/steppe_grass.glb",
+      function (gltf) {
+        gltf.scene.position.set(0, -2.35, 0);
+        scene.add(gltf.scene);
+      },
+      undefined,
+      function (error) {
+        console.error(error);
+      }
+    );
+
+    const light = new THREE.PointLight(0xffffff, 4.2, 100);
+    light.position.set(20, 20, 40);
     scene.add(light);
+
+    const light2 = new THREE.PointLight(0xffffff, 3, 100);
+    light2.position.set(-20, 20, -40);
+    scene.add(light2);
 
     const renderer = new THREE.WebGLRenderer();
     renderer.setClearColor(0xffffff, 1);
@@ -37,13 +58,12 @@ function Page({ params }: { params: { repoId: string } }) {
 
     if (canvas.current) {
       camera = new THREE.PerspectiveCamera(
-        75,
-        canvas.current.clientWidth / canvas.current.clientHeight,
-        0.1,
-        1000
+        80,
+        canvas.current.clientWidth / canvas.current.clientHeight
       );
 
-      camera.position.z = 5;
+      camera.position.y = 0.5;
+      camera.position.z = 1;
 
       renderer.setSize(canvas.current.clientWidth, canvas.current.clientHeight);
 
@@ -73,13 +93,58 @@ function Page({ params }: { params: { repoId: string } }) {
         <div className={styles["repo-mon-div"]}>
           <div ref={canvas} className={styles["repo-mon"]}></div>
           <div className={styles.exp}>
-            <div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
-              <div
-                className="bg-blue-600 h-2.5 rounded-full"
-                style={{ width: "45%" }}
-              ></div>
+            <p>폭스</p>
+            <ProgressBar />
+          </div>
+        </div>
+        <div className={styles["default-info-div"]}>
+          <div className={styles.first}>
+            <span className={styles.title}>Repo Name</span>
+            <div className={styles["icon-div"]}>
+              <span className={styles.star}>
+                <StarIcon
+                  width="1.25rem"
+                  style={{ display: "inline", marginRight: "0.2rem" }}
+                />
+                12
+              </span>
+              <span className={styles.share}>
+                <ShareIcon
+                  width="1.25rem"
+                  style={{ display: "inline", marginRight: "0.2rem" }}
+                />
+                15
+              </span>
             </div>
           </div>
+          <p className={styles.date}>23.02.20 ~ 23.04.07</p>
+          <div className={styles["lan-div"]}>
+            <span>TypeScript</span>
+            <span>Java</span>
+            <span>Python</span>
+            <span>TypeScript</span>
+            <span>Java</span>
+            <span>Python</span>
+            <span>TypeScript</span>
+            <span>Java</span>
+            <span>Python</span>
+          </div>
+          <div className={styles["tag-div"]}>
+            <span>KakaoAPI</span>
+            <span>GithubAPI</span>
+            <span>NextJS</span>
+            <span>ThreeJS</span>
+            <span>SpringBoot</span>
+            <span>KakaoAPI</span>
+            <span>GithubAPI</span>
+            <span>NextJS</span>
+            <span>ThreeJS</span>
+            <span>SpringBoot</span>
+          </div>
+          <p className={styles.des}>
+            Repo Description Repo Description Repo Description Repo Description
+            Repo Description Repo Description
+          </p>
         </div>
       </div>
     </div>
