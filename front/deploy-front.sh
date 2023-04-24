f66df1b06352a8f64e3f4eca7695b76156334deb
안명수 <ddings7303@gmail.com>
2023-04-24 20:36:55 +0900

    [infra] Rename: nginx ssl 설정 추가

--- front/deploy-front.sh
+++ front/deploy-front.sh
@@ -1,4 +1,4 @@
 
 sudo docker ps -a -q --filter "name=front" | grep -q . && docker stop front && docker rm front | true
-docker run -d -p 80:80 -p 443:443 -v /home/ubuntu/nginx/conf.d:/etc/nginx/conf.d --name front e105/front:1.0
+docker run -d -p 80:80 -p 443:443 -v /home/ubuntu/dev/conf.d:/etc/nginx/conf.d -v /home/ubuntu/certbot/conf:/etc/letsencrypt/ -v /home/ubuntu/certbot/www:/var/www/certbot --name front e105/front:1.0
 docker image prune -af
