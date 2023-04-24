d9d449570c249aadf2b986c95e113e9a00d7ab70
eunjin <yej6642@naver.com>
2023-04-24 12:01:51 +0900

    Feat : 추가 swagger 설정

--- back/src/main/java/com/repomon/rocketdan/domain/user/controller/RankController.java
+++ back/src/main/java/com/repomon/rocketdan/domain/user/controller/RankController.java
@@ -3,6 +3,7 @@ package com.repomon.rocketdan.domain.user.controller;
 import com.repomon.rocketdan.domain.repo.dto.RepoRankResponseDto;
 import com.repomon.rocketdan.domain.repomon.dto.RepomonRankResponseDto;
 import com.repomon.rocketdan.domain.user.dto.UserRankResponseDto;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,17 +12,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/rank")
 public class RankController {
-
+    @ApiOperation(value = "유저 랭킹 조회")
     @GetMapping("/user")
     public ResponseEntity<UserRankResponseDto> getUserRankList(){
         return ResponseEntity.ok().build();
     }
-
+    @ApiOperation(value = "레포 랭킹 조회")
     @GetMapping("/repo")
     public ResponseEntity<RepoRankResponseDto> getRepoRankList(){
         return ResponseEntity.ok().build();
     }
-
+    @ApiOperation(value = "전투 랭킹 조회")
     @GetMapping("/repomon")
     public ResponseEntity<RepomonRankResponseDto> getRepomonRankList(){
         return ResponseEntity.ok().build();
