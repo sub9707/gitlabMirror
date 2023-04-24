9cbc3cee4fe5185e79b90cb4b9806bb905d532c6
Kminis <hello11robot@gmail.com>
2023-04-24 10:55:51 +0900

    [BE] feat : Repomon 관련 Controller, Service, Entity 작성
    - Entity에 스탯 관련 요소 추가
    - Controller 초기 구조 설정
    - Service 파일 생성

---
+++ back/src/main/java/com/repomon/rocketdan/domain/repomon/service/RepomonService.java
@@ -0,0 +1,13 @@
+package com.repomon.rocketdan.domain.repomon.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@RequiredArgsConstructor
+public class RepomonService {
+
+    
+
+}
