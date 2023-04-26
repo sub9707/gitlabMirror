e45bdedbcc10d6f0928c91221b3f611f864bf531
Kminis <hello11robot@gmail.com>
2023-04-26 12:12:56 +0900

    [BE] feat : 레포몬 게임 관련 기능 구현 - 스텟 수정 - 매칭 기능 - 전투 기능 이외 작성

--- back/src/main/java/com/repomon/rocketdan/domain/repomon/repository/BattleLogRepository.java
+++ back/src/main/java/com/repomon/rocketdan/domain/repomon/repository/BattleLogRepository.java
@@ -1,7 +1,19 @@
 package com.repomon.rocketdan.domain.repomon.repository;
 
+
 import com.repomon.rocketdan.domain.repomon.entity.BattleLogEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
 
 public interface BattleLogRepository extends JpaRepository<BattleLogEntity, Long> {
+
+	@Query(nativeQuery = true, value = "SELECT * FROM battle_log " +
+		"where attack_repo_id = :repo_id or defense_repo_id = :repo_id " +
+		"ORDER BY created_at DESC LIMIT 5 ")
+	List<BattleLogEntity> findTop5ByRepoIdOrderByDesc(@Param("repo_id") Long repoId);
+
 }
--- back/src/main/java/com/repomon/rocketdan/domain/repomon/repository/RepomonStatusRepository.java
+++ back/src/main/java/com/repomon/rocketdan/domain/repomon/repository/RepomonStatusRepository.java
@@ -1,5 +1,6 @@
 package com.repomon.rocketdan.domain.repomon.repository;
 
+
 import com.repomon.rocketdan.domain.repomon.entity.RepomonStatusEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -10,15 +11,15 @@ import java.util.Optional;
 
 public interface RepomonStatusRepository extends JpaRepository<RepomonStatusEntity, Long> {
 
-//    // Rating이 가장 가까운 값 추출하기
-//    @Query("select r from RepomonStatusEntity r order by abs(r.rating- :rating)")
-//    Page<RepomonStatusEntity> findByRatingOrderByAbsRating(@Param("rating") Integer rating, Pageable pageable);
+	Optional<RepomonStatusEntity> findByRepoId(Long id);
 
-    // 범위 안에서 랜덤으로 한 가지 추출하기
-    @Query(nativeQuery = true, value = "SELECT * FROM rocketdan.repomon_status_entity INNER JOIN rocketdan.repo_entity " +
-            "ON rocketdan.repomon_status_entity.repo_id = rocketdan.repo_entity.repo_id " +
-            "WHERE rocketdan.repomon_status_entity.rating >= :startRating and rocketdan.repomon_status_entity.rating < :endRating and rocketdan.repomon_status_entity.repo_id != :repoId Order By RAND() limit 1 ")
-    Optional<RepomonStatusEntity> findByRatingBetweenRandom(@Param("startRating") Integer startRating, @Param("endRating") Integer endRating, @Param("repoId") Long repoId);
-
+	// 범위 안에서 랜덤으로 한 가지 추출하기
+	@Query(nativeQuery = true,
+		value = "SELECT * FROM repomon_status INNER JOIN repo " +
+			"ON repomon_status.repo_id = repo.repo_id " +
+			"WHERE repomon_status.rating >= :startRating and repomon_status.rating < :endRating and repomon_status.repo_id != :repoId ORDER BY RAND() limit 1 ")
+	Optional<RepomonStatusEntity> findByRatingBetweenRandom(
+		@Param("startRating") Integer startRating, @Param("endRating") Integer endRating,
+		@Param("repoId") Long repoId);
 
 }
