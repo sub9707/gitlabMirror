43190f9f681547cab855003c3a71ad12e9457098
백준봉 <bjb2992@naver.com>
2023-04-27 10:07:20 +0900

    Design : 메인 페이지 디자인 보완

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,16 +1,43 @@
 "use client";
 
 import Image from "next/image";
-import React, { useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import styles from "./page.module.scss";
 import grow from "../../public/service_grow.png";
 import fight from "../../public/service_fight.png";
 import card from "../../public/service_card.png";
 
 const Home = () => {
+  const [scrollY, setScrollY] = useState<number>(0);
+  const fpRef = useRef<HTMLImageElement>(null);
+  const spRef = useRef<HTMLImageElement>(null);
+  const tpRef = useRef<HTMLImageElement>(null);
+
   useEffect(() => {
-    console.log(localStorage.getItem("accessToken"));
+    const handleScroll = () => {
+      setScrollY(window.scrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
   }, []);
+
+  useEffect(() => {
+    if (fpRef.current && spRef.current && tpRef.current) {
+      if (scrollY < 550) {
+        fpRef.current.style.opacity = "0";
+        spRef.current.style.opacity = "0";
+        tpRef.current.style.opacity = "0";
+      } else {
+        fpRef.current.style.opacity = "1";
+        spRef.current.style.opacity = "1";
+        tpRef.current.style.opacity = "1";
+      }
+    }
+  }, [scrollY]);
 
   return (
     <div className={styles.container}>
@@ -27,11 +54,17 @@ const Home = () => {
         <h2 className={styles["service-title"]}>
           <span>레포몬</span> 서비스 둘러보기
         </h2>
+
         <div className={styles["service-content"]}>
           <div className={styles["img-div"]} style={{ margin: "0 20px 0 0" }}>
-            <Image alt="grow" src={grow} className={styles.fp}></Image>
+            <Image
+              alt="grow"
+              src={grow}
+              className={styles.fp}
+              ref={fpRef}
+            ></Image>
             <span>레포지토리 분석</span>
-            <p className={styles.des}>
+            <p className={styles.des} style={{ padding: "4rem 1rem" }}>
               당신의 Github 레포지토리와 함께 성장하는 레포몬을 육성해보세요.
               <br />
               분석 차트와 함께 당신의 프로젝트를 한눈에 시각화해서 나타내줘요.
@@ -39,7 +72,12 @@ const Home = () => {
           </div>
           <div>
             <div className={styles["img-div"]} style={{ margin: "0 0 20px 0" }}>
-              <Image alt="grow" src={fight} className={styles.sp}></Image>
+              <Image
+                alt="grow"
+                src={fight}
+                className={styles.sp}
+                ref={spRef}
+              ></Image>
               <span>배틀 및 랭킹</span>
               <p className={styles.des}>
                 당신의 프로젝트가 얼마나 강한지 궁금하지 않나요?
@@ -48,7 +86,12 @@ const Home = () => {
               </p>
             </div>
             <div className={styles["img-div"]}>
-              <Image alt="grow" src={card} className={styles.tp}></Image>
+              <Image
+                alt="grow"
+                src={card}
+                className={styles.tp}
+                ref={tpRef}
+              ></Image>
               <span>카드 추출</span>
               <p className={styles.des}>
                 열심히 성장시킨 내 프로젝트를
