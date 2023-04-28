42724bf7191e25cffa503da005907f73a346c6b9
Kminis <hello11robot@gmail.com>
2023-04-28 11:42:36 +0900

    [BE] cleanup : 레포지토리 추가

--- back/src/main/java/com/repomon/rocketdan/domain/repo/repository/ActiveRepoRepository.java
+++ back/src/main/java/com/repomon/rocketdan/domain/repo/repository/ActiveRepoRepository.java
@@ -1,13 +1,20 @@
 package com.repomon.rocketdan.domain.repo.repository;
 
+
+import com.repomon.rocketdan.domain.repo.entity.RepoEntity;
 import com.repomon.rocketdan.domain.user.entity.ActiveRepoEntity;
 import com.repomon.rocketdan.domain.user.entity.UserEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
+
 public interface ActiveRepoRepository extends JpaRepository<ActiveRepoEntity, Long> {
 
-    Page<ActiveRepoEntity> findByUser(UserEntity userEntity, Pageable pageable);
-    Long countByUser_UserId(Long userId);
+	Page<ActiveRepoEntity> findByUser(UserEntity userEntity, Pageable pageable);
+	Optional<ActiveRepoEntity> findByRepoAndUser(RepoEntity repoEntity, UserEntity userEntity);
+	Long countByUser_UserId(Long userId);
+
 }
