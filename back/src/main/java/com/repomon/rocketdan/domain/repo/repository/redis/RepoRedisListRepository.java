bda99a5aabd37b7b2a99ab31f882d5bc194856c4
ddings <ddings7303@gmail.com>
2023-05-01 17:28:54 +0900

    [BE] Fix: import 안쓰는거 제거

--- back/src/main/java/com/repomon/rocketdan/domain/repo/repository/redis/RepoRedisListRepository.java
+++ back/src/main/java/com/repomon/rocketdan/domain/repo/repository/redis/RepoRedisListRepository.java
@@ -2,7 +2,6 @@ package com.repomon.rocketdan.domain.repo.repository.redis;
 
 import com.repomon.rocketdan.domain.repo.dto.response.RepoListResponseDto;
 import java.util.List;
-import java.util.Optional;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
