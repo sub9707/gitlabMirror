924454c675e1a006fb9cb38c9c184816281934e3
ddings <ddings7303@gmail.com>
2023-05-01 17:27:17 +0900

    [BE] Feature: repoList 조회 redis 추가

--- back/src/main/java/com/repomon/rocketdan/domain/repo/dto/response/RepoListResponseDto.java
+++ back/src/main/java/com/repomon/rocketdan/domain/repo/dto/response/RepoListResponseDto.java
@@ -1,35 +1,50 @@
 package com.repomon.rocketdan.domain.repo.dto.response;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.repomon.rocketdan.domain.repo.app.RepoDetail;
 import com.repomon.rocketdan.domain.repo.app.RepoListItem;
+import java.util.ArrayList;
+import javax.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.index.Indexed;
 
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@RedisHash(value = "redis-repoList")
 public class RepoListResponseDto {
 
+    @Id @JsonIgnore
+    private Long id;
+    @Indexed
+    @JsonIgnore
+    private String userName;
     private int totalPages;
     private long totalElements;
     private List<RepoListItem> repoListItems;
 
-
-    public static RepoListResponseDto fromDetails(List<RepoDetail> repoDetails, long totalElements, int totalPages) {
-        List<RepoListItem> collect = repoDetails.stream().map(RepoListItem::convertFromDetail)
-            .collect(Collectors.toList());
-        return new RepoListResponseDto(totalPages, totalElements, collect);
+    public static RepoListResponseDto empty(String userName){
+        return new RepoListResponseDto(null, userName, 0, 0L, new ArrayList<>());
     }
 
+    public void updateFromDetails(List<RepoDetail> repoDetails, long totalElements, int totalPages) {
+        List<RepoListItem> collect = repoDetails.stream()
+            .map(RepoListItem::convertFromDetail)
+            .collect(Collectors.toList());
 
-    public RepoListItem getRepoItem(RepoDetail repoDetail) {
-        return RepoListItem.convertFromDetail(repoDetail);
+        this.totalPages = totalPages;
+        this.totalElements = totalElements;
+        this.repoListItems = collect;
     }
 
 }
+
+
---
+++ back/src/main/java/com/repomon/rocketdan/domain/repo/repository/redis/RepoRedisListRepository.java
@@ -0,0 +1,12 @@
+package com.repomon.rocketdan.domain.repo.repository.redis;
+
+import com.repomon.rocketdan.domain.repo.dto.response.RepoListResponseDto;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RepoRedisListRepository extends CrudRepository<RepoListResponseDto, Long> {
+
+    List<RepoListResponseDto> findByUserName(String userName, Pageable pageable);
+}
