28fa1c0125129589269a75c4650a6bc3bfde7e0c
백준봉 <bjb2992@naver.com>
2023-05-02 14:16:44 +0900

    Feature: 로그인, 로그아웃, 리프레쉬

--- front/src/api/axios.ts
+++ front/src/api/axios.ts
@@ -1,6 +1,52 @@
-import axios, { AxiosInstance } from "axios";
+import axios, { AxiosHeaders, AxiosInstance } from "axios";
+import { axiosRequestRefresh } from "./auth";
 
 export const http: AxiosInstance = axios.create({
-  baseURL: "https://k8e105.p.ssafy.io/api/v1",
+  baseURL: "https://repomon.kr/api/v1",
   // withCredentials: true,
 });
+
+http.interceptors.request.use(
+  function (config) {
+    config.headers.Authorization = `Bearer ${localStorage.getItem(
+      "accessToken"
+    )}`;
+    return config;
+  },
+  function (err) {
+    // 요청 오류가 있는 작업 수행
+    return Promise.reject(err);
+  }
+);
+
+http.interceptors.response.use(
+  function (res) {
+    return res;
+  },
+  async function (err) {
+    if (err.response && err.response.status === 401) {
+      const refreshToken = localStorage.getItem("refreshToken");
+      if (!refreshToken) {
+        return err;
+      }
+      try {
+        const res = await axiosRequestRefresh();
+        const headers = res.headers as AxiosHeaders;
+
+        localStorage.setItem(
+          "accessToken",
+          headers.get("accessToken") as string
+        );
+        localStorage.setItem(
+          "refreshToken",
+          headers.get("refreshToken") as string
+        );
+        return await http.request(err.config);
+      } catch (err) {
+        console.error(err);
+      }
+      return Promise.reject(err);
+    }
+    return Promise.reject(err);
+  }
+);
