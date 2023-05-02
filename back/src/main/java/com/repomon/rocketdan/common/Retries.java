1bbf8d47a3360139b7d756f728e14dcba9e7f2d6
eunjin <yej6642@naver.com>
2023-05-02 15:02:08 +0900

    Feat : 수정 contributers

---
+++ back/src/main/java/com/repomon/rocketdan/common/Retries.java
@@ -0,0 +1,9 @@
+package com.repomon.rocketdan.common;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.CLASS)
+public @interface Retries {
+
+}
