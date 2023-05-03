9434aebd9b63f73602049ae6b895ef708cac1232
sub9707 <110171787+sub9707@users.noreply.github.com>
2023-05-03 10:02:34 +0900

    feat main gitData

--- front/src/app/loading.tsx
+++ front/src/app/loading.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { gitTipData } from "./dashboard/gitData";
 
 function Loading() {
   return (
@@ -7,11 +8,15 @@ function Loading() {
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
+        flexDirection: "column",
         width: "100%",
         height: "100vh",
       }}
     >
       <iframe src="https://embed.lottiefiles.com/animation/102785" />
+      <p style={{ fontSize: "1.2em", marginTop: "1em", fontWeight: "700" }}>
+        RepoMon~
+      </p>
     </div>
   );
 }
