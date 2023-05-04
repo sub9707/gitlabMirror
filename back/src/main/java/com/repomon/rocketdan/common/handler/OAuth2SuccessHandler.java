9db1e43ce6268021a21439cf3cc9d9f3b93c8069
SeungtaeRyu <dbtmdxo1992@naver.com>
2023-05-04 15:06:17 +0900

    [BE] Feat : 깃허브 로그인 prod / dev 분기

--- back/src/main/java/com/repomon/rocketdan/common/handler/OAuth2SuccessHandler.java
+++ back/src/main/java/com/repomon/rocketdan/common/handler/OAuth2SuccessHandler.java
@@ -4,6 +4,7 @@ package com.repomon.rocketdan.common.handler;
 import com.repomon.rocketdan.common.dto.AuthResponseDto;
 import com.repomon.rocketdan.common.service.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
@@ -18,7 +19,11 @@ import java.io.IOException;
 @Component
 @RequiredArgsConstructor
 public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
 	private final JwtTokenProvider authTokenProvider;
+	@Value("${front_url}")
+	private String frontUrl;
+
 
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
@@ -34,7 +39,9 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
 		String avatarUrl = oAuth2User.getAttribute("avatarUrl");
 		AuthResponseDto authResponseDto = authTokenProvider.createToken(userId);
 
-		setDefaultTargetUrl("http://localhost:3000?access-token=" + authResponseDto.getAccessToken() + "&refresh-token=" + authResponseDto.getRefreshToken() + "&username=" + username + "&name=" + name + "&avatarUrl=" + avatarUrl + "&userId=" + userId);
+		setDefaultTargetUrl(
+			frontUrl + "?access-token=" + authResponseDto.getAccessToken() + "&refresh-token=" + authResponseDto.getRefreshToken() + "&username=" + username + "&name=" + name + "&avatarUrl="
+				+ avatarUrl + "&userId=" + userId);
 		super.onAuthenticationSuccess(request, response, authentication);
 
 	}
