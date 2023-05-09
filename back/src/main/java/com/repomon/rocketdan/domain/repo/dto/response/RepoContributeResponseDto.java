edf25e8d514a03fcb5cc6513609e97136a20af1b
ddings <ddings7303@gmail.com>
2023-05-09 14:07:35 +0900

    [BE] Feat: convention, contribute redis null처리 개선

--- back/src/main/java/com/repomon/rocketdan/domain/repo/dto/response/RepoContributeResponseDto.java
+++ back/src/main/java/com/repomon/rocketdan/domain/repo/dto/response/RepoContributeResponseDto.java
@@ -8,11 +8,13 @@ import javax.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.index.Indexed;
 
 @Getter @Builder
 @RedisHash(value = "repo-contribute", timeToLive = 86400)
+@NoArgsConstructor
 @AllArgsConstructor
 public class RepoContributeResponseDto {
 
@@ -23,12 +25,14 @@ public class RepoContributeResponseDto {
     private String repoOwner;
     private int totalCommitCount;
     private long totalLineCount;
-    private Map<String, Integer> committers = new HashMap<>();
+    private Map<String, Integer> committers;
     private String mvp;
 
 
     public static RepoContributeResponseDto of(int totalCommitCount, long totalLineCount,
         Map<String, Integer> committers, String mvp, RepoEntity repoEntity) {
+        committers = committers.isEmpty() ? new HashMap<>() : committers;
+
         return RepoContributeResponseDto.builder()
             .totalCommitCount(totalCommitCount)
             .totalLineCount(totalLineCount)
--- back/src/main/java/com/repomon/rocketdan/domain/repo/dto/response/RepoConventionResponseDto.java
+++ back/src/main/java/com/repomon/rocketdan/domain/repo/dto/response/RepoConventionResponseDto.java
@@ -6,7 +6,9 @@ import com.repomon.rocketdan.domain.repo.entity.RepoConventionEntity;
 import com.repomon.rocketdan.domain.repo.entity.RepoEntity;
 import java.util.ArrayList;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.index.Indexed;
 
@@ -15,8 +17,9 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 
-@Getter
+@Getter @Builder
 @RedisHash(value = "repo-convention", timeToLive = 86400)
+@NoArgsConstructor
 @AllArgsConstructor
 public class RepoConventionResponseDto {
 
@@ -34,10 +37,18 @@ public class RepoConventionResponseDto {
 	public static RepoConventionResponseDto fromEntities(RepoEntity repoEntity, List<RepoConventionEntity> conventions,
 		int totalCnt, int collectCnt) {
 
-		List<ConventionInfo> collect = conventions.stream().map(ConventionInfo::of)
-			.collect(Collectors.toList());
+		List<ConventionInfo> collect = conventions.isEmpty() ? new ArrayList<>()
+			:conventions.stream()
+				.map(ConventionInfo::of)
+				.collect(Collectors.toList());
 
-		return new RepoConventionResponseDto(null, repoEntity.getRepoId(), repoEntity.getRepoOwner(), collect, totalCnt, collectCnt);
+		return RepoConventionResponseDto.builder()
+			.repoId(repoEntity.getRepoId())
+			.repoOwner(repoEntity.getRepoOwner())
+			.conventions(collect)
+			.totalCnt(totalCnt)
+			.collectCnt(collectCnt)
+			.build();
 	}
 
 
