0fa4494819d3d6e46a1c3e7abe3aba2bc06885bb
김승섭 <sub9707@naver.com>
2023-05-11 09:46:54 +0900

    fix 버튼 오류, ui props 추가

--- front/src/app/repo/[repoId]/registRepo/page.tsx
+++ front/src/app/repo/[repoId]/registRepo/page.tsx
@@ -26,6 +26,7 @@ import InputField from "@/components/UI/InputField";
 import { RepoInitType } from "@/types/repoRegist";
 import Modal from "react-modal";
 import { useRouter } from "next/navigation";
+import ArrowDown from "@/components/UI/ArrowDown";
 
 const Page: NextPage<PageProps> = ({ params }) => {
   const [numArr, setNumArr] = useState([0, 0, 0, 0, 0]);
@@ -69,12 +70,6 @@ const Page: NextPage<PageProps> = ({ params }) => {
       }, 2000);
     }
   }
-  useEffect(() => {
-    console.log(repoInitData.repomonId);
-    if (repoInitData.repomonId <= 9000) {
-      // alert("이미 등록된 레포몬 입니다");
-    }
-  }, []);
 
   useEffect(() => {
     setRepoInitData({
@@ -217,8 +212,6 @@ const Page: NextPage<PageProps> = ({ params }) => {
   const [isHoveredOne, setIsHoveredOne] = useState<boolean>(false);
   const [isHoveredTwo, setIsHoveredTwo] = useState<boolean>(false);
   const [isHoveredThree, setIsHoveredThree] = useState<boolean>(false);
-  const [selectedRef, setSelectedRef] =
-    useState<React.RefObject<HTMLDivElement>>();
   const monRef1 = useRef<HTMLDivElement>(null);
   const monRef2 = useRef<HTMLDivElement>(null);
   const monRef3 = useRef<HTMLDivElement>(null);
@@ -328,7 +321,16 @@ const Page: NextPage<PageProps> = ({ params }) => {
         </p>
       </Modal>
       <div className={styles.selectBox}>
-        <p className={styles.repoTitle}>함께할 레포몬을 선택해주세요!</p>
+        <p className={styles.repoTitle}>
+          {isClickOne || isClickTwo || isClickThree ? (
+            <div style={{ display: "flex" }}>
+              <ArrowDown num={12} /> <p>아래로 이동해 특성을 설정하세요</p>
+              <ArrowDown num={12} />
+            </div>
+          ) : (
+            "함께할 레포몬을 선택해주세요!"
+          )}
+        </p>
         <div style={{ display: "flex" }}>
           <div
             className={styles.monChar}
--- front/src/components/Skeletons/LoadingSpinner.tsx
+++ front/src/components/Skeletons/LoadingSpinner.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 
-function LoadingSpinner() {
+type SpinnerProps = {
+  ml: number;
+  mr: number;
+  size: number;
+};
+
+function LoadingSpinner(props: SpinnerProps) {
   return (
-    <div role="status" className="flex items-center justify-center ml-4">
+    <div
+      role="status"
+      className={`flex items-center justify-center ml-${props.ml}`}
+    >
       <svg
         aria-hidden="true"
-        className="inline w-6 h-6 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+        className={`inline w-${props.size} h-${props.size} mr-${props.mr} text-gray-200 animate-spin dark:text-gray-600 fill-blue-600`}
         viewBox="0 0 100 101"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
