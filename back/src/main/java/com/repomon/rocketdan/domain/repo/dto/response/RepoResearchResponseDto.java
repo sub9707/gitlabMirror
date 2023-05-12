95a31df3c7d7956bf092d55b906454c23c113995
ddings <ddings7303@gmail.com>
2023-05-12 17:31:11 +0900

    [BE] Feat: 분석조회 date단위로 묶어서 보냄

--- back/src/main/java/com/repomon/rocketdan/domain/repo/dto/response/RepoResearchResponseDto.java
+++ back/src/main/java/com/repomon/rocketdan/domain/repo/dto/response/RepoResearchResponseDto.java
@@ -3,10 +3,12 @@ package com.repomon.rocketdan.domain.repo.dto.response;
 import com.repomon.rocketdan.domain.repo.app.GrowthFactor;
 import com.repomon.rocketdan.domain.repo.entity.RepoHistoryEntity;
 import java.time.LocalDate;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -23,18 +25,18 @@ public class RepoResearchResponseDto {
     private Long repoExp;
     private Long totalGetExp;
     private Map<String, Long> growthFactor;
-    private List<HistoryInfo> histories;
+    private Map<LocalDate, Long> histories;
 
     public static RepoResearchResponseDto fromHistoryAndRank(
         List<RepoHistoryEntity> historyEntityList, Long rank, Long repoExp) {
 
-        List<HistoryInfo> histories = historyEntityList.stream()
+        List<HistoryInfo> historyInfos = historyEntityList.stream()
             .map(HistoryInfo::fromEntity)
             .collect(Collectors.toList());
 
         Long totalGetExp = 0L;
         Map<String, Long> growthFactor = new HashMap<>();
-        for(HistoryInfo historyInfo : histories){
+        for(HistoryInfo historyInfo : historyInfos){
             totalGetExp += historyInfo.exp;
 
             if(growthFactor.containsKey(historyInfo.type)){
@@ -45,7 +47,15 @@ public class RepoResearchResponseDto {
             }
         }
 
-        histories.sort(Comparator.comparing(HistoryInfo::getWorkedAt));
+        Map<LocalDate, Long> histories = new TreeMap<>(LocalDate::compareTo);
+        historyInfos.forEach(historyInfo -> {
+            LocalDate workedAt = historyInfo.getWorkedAt();
+            if(histories.containsKey(workedAt)){
+                histories.replace(workedAt, histories.get(workedAt) + historyInfo.exp);
+            }else{
+                histories.put(workedAt, historyInfo.exp);
+            }
+        });
 
         return RepoResearchResponseDto.builder()
             .rank(rank)
@@ -63,7 +73,6 @@ public class RepoResearchResponseDto {
         private Long exp;
         private String type;
         private LocalDate workedAt;
-
         public static HistoryInfo fromEntity(RepoHistoryEntity historyEntity){
             Integer repoHistoryType = historyEntity.getRepoHistoryType();
             return new HistoryInfo(historyEntity.getRepoHistoryExp(), GrowthFactor.idxToEnum(repoHistoryType).name(), historyEntity.getWorkedAt());
