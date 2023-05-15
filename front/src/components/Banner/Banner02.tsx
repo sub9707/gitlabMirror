cafc1d963918087b919ebc39d91888fdb64ae9bd
SeungtaeRyu <dbtmdxo1992@naver.com>
2023-05-15 10:19:18 +0900

    [FE] Cleanup : 배너 글자 폰트사이즈 수정

--- front/src/components/Banner/Banner02.tsx
+++ front/src/components/Banner/Banner02.tsx
@@ -12,7 +12,7 @@ const Banner02 = () => {
           </div>
           <div className={`${styles.left} flex flex-col items-end`}>
             <p className={styles.title}>레포지토리를 분석하고,</p>
-            <p className='text-5xl'>README 에 붙일 수 있어요 !</p>
+            <p className={`${styles.desk125text3xl}`}>README 에 붙일 수 있어요 !</p>
             <p className='text-xl py-10'>유저 카드, 협업 레포지토리 카드, 개인 레포지토리 카드 추출이 가능해요 😊</p>
           </div>
         </div>
