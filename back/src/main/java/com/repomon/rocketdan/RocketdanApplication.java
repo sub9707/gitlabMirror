e07829fca7d6246069a9c8f69ee3927195a48e11
ddings <ddings7303@gmail.com>
2023-05-22 10:43:37 +0900

    [BE] Fix: spring timezone 설정 삭제

--- back/src/main/java/com/repomon/rocketdan/RocketdanApplication.java
+++ back/src/main/java/com/repomon/rocketdan/RocketdanApplication.java
@@ -11,10 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RocketdanApplication {
 
 	public static void main(String[] args) {
-		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 		SpringApplication.run(RocketdanApplication.class, args);
-
-		log.info("TimeZone:", TimeZone.getDefault().getID());
 	}
 
 }
